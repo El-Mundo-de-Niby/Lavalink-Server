53bc108c522e5b04973bb00139735352fae2de87
dewstouh <diegorodriguezpastor383@gmail.com>
2023-08-27 14:34:14 +0200

    Server Configurado de Lavalink

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  apps: [
+    {
+      name: path.dirname(__filename).split(path.sep).pop(), // Nombre del directorio actual
+      script: 'java',
+      args: ['-jar', 'Lavalink.jar'],
+      // eslint-disable-next-line camelcase
+      cron_restart: '0 0 * * *', // A las 00:00
+      env: {
+        NODE_OPTIONS: '--no-warnings=ExperimentalWarning',
+      },
+    },
+  ],
+};
